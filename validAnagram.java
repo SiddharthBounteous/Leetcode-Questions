95c53d5df282c6809de83cd8b02f92ac8bb8ce1d
SiddharthBounteous <siddharth.shukla@bounteous.com>
2026-03-21 19:53:19 +0530

    Create validAnagram.java

---
+++ validAnagram.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int arr[]=new int[26];
+
+        int n=s.length();
+        int m=t.length();
+
+        if(n!=m){
+            return false;
+        }
+
+        for(int i=0;i<n;i++){
+            arr[s.charAt(i)-'a']++;
+        }
+
+        for(int i=0;i<m;i++){
+            arr[t.charAt(i)-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(arr[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
