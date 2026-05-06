cd4f399129cd614724244e227ade9d4bf0b677a9
SiddharthBounteous <siddharth.shukla@bounteous.com>
2026-05-06 09:41:14 +0530

    Create BinarySubarrayWithSum.java

---
+++ BinarySubarrayWithSum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solve(int [] nums,int goal){
+        if(goal<0){
+            return 0;
+        }
+        int l=0,r=0;
+
+        int count=0,sum=0;
+
+        while(r<nums.length){
+            sum+=nums[r];
+
+            while(sum>goal){
+                sum=sum-nums[l];
+                l++;
+            }
+
+            count=count+(r-l+1);
+            r++;
+        }
+
+        return count;
+    }
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return solve(nums,goal)-solve(nums,goal-1);
+    }
+}
